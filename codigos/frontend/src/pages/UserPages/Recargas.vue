eff820581353554fc37e0c5d97a770b1e44f7f5e
João Victor Costa <joaovictorca16@gmail.com>
2020-08-08 17:06:01 -0300

    Lista de recargas

--- codigos/frontend/src/pages/UserPages/Recargas.vue
+++ codigos/frontend/src/pages/UserPages/Recargas.vue
@@ -1,12 +1,58 @@
 <template>
-  <q-page>
-      Recargas
+  <q-page padding>
+    <q-table
+      class="q-mt-lg"
+      title="Recargas"
+      :data="recargas"
+      :columns="columns"
+      row-key="id"
+      hide-pagination
+      no-data-label="Sem resultados"
+      :rows-per-page-options="[0]"
+      flat
+      bordered
+    >
+    </q-table>
   </q-page>
 </template>
 
 <script>
+import service from '../../services/recarga'
+import { showError } from 'src/global'
+
 export default {
-  name: 'Recargas'
+  name: 'Recargas',
+  data () {
+    return {
+      cliente: null,
+      recargas: [],
+      columns: [
+        {
+          name: 'cartaoInteligente',
+          label: 'Cartão Inteligente',
+          align: 'left',
+          field: recarga => recarga.cartaoInteligente.nome,
+          sortable: true
+        },
+        { name: 'valor', label: 'Valor', align: 'left', field: 'valor', sortable: true },
+        { name: 'data', label: 'Data', align: 'left', field: 'data', sortable: true },
+        { name: 'status', label: 'Status', align: 'left', field: 'status', sortable: true }
+      ]
+    }
+  },
+  methods: {
+    loadRecargas () {
+      service.findByCliente(this.cliente)
+        .then(res => {
+          this.recargas = res.data
+        })
+        .catch(showError)
+    }
+  },
+  mounted () {
+    this.cliente = { id: 2 }
+    this.loadRecargas()
+  }
 }
 </script>
 
