0301b45f4dc23a292326639b185ece4156ccc702
João Victor Costa <joaovictorca16@gmail.com>
2020-08-15 10:34:52 -0300

    Backend: autenticação e autorização com o basic auth do pacote spring security

--- codigos/frontend/src/services/http.js
+++ codigos/frontend/src/services/http.js
@@ -2,8 +2,11 @@ import axios from 'axios'
 
 const client = axios.create({
   baseURL: 'http://localhost:8081/',
-  timeout: 1000
-  // headers: { 'X-Custom-Header' : 'foobar' }
+  timeout: 1000,
+  headers: { // Teste
+    Authorization: 'Basic dXN1YXJpbzAxQG1haWwuY29tOjAxMjM0NTY3'
+    // Cookie: 'JSESSIONID=7B859CCC80083BFF777DAB0A7C6139FA'
+  }
 })
 
 export default client
